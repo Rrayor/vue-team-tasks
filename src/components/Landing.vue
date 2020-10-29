2a95adbf2e72af97c63d25271f150a609aabc9b2
Rrayor <simon.benjamin93@gmail.com>
2020-10-29 13:46:10 +0100

    worked on landing and register templates

---
+++ src/components/Landing.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <h1>Welcome</h1>
+    <Register />
+  </v-container>
+</template>
+
+<script>
+import Register from './Register'
+export default {
+  name: 'Landing',
+  components: {
+    Register
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Register.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card class="mx-auto my-12" max-width="50%">
+    <v-container>
+      <v-card-title> Register </v-card-title>
+      <v-card-text>
+        <v-form>
+          <v-text-field label="E-mail"></v-text-field>
+          <v-text-field label="Password"></v-text-field>
+          <v-btn color="primary">Register</v-btn>
+        </v-form>
+      </v-card-text>
+    </v-container>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'Register'
+}
+</script>
+
+<style>
+</style>
